1535386591d1446415a97c0bf1c95140230762f1
nicolas kobel <nicolas.kobel@hes-so.ch>
2023-09-12 09:42:42 +0200

    Refractored Animation to use Tree

--- unit.gd
+++ unit.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
-@export var SPEED = 200.0
+@export var SPEED : float = 200.0
+@export var starting_direction : Vector2 = Vector2(0,1)
 
 @onready var _animation_player = $AnimationPlayer
+
+@onready var _animation_tree = $AnimationTree
+@onready var _animation_state_machine = _animation_tree.get("parameters/playback")
 
 enum direction_enum {UP, DOWN, LEFT, RIGHT}
 
 var direction
+
+# parameters/Idle/blend_position
+
+func _ready():
+	_update_animation(starting_direction)
 
 func _physics_process(delta):
 
@@ -15,54 +24,24 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var x_direction = Input.get_axis("ui_left", "ui_right")
 	var y_direction = Input.get_axis("ui_up", "ui_down")
-	if x_direction:
-		velocity.x = x_direction * SPEED
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	_update_animation(x_direction,y_direction)
+	
+	velocity.x = x_direction * SPEED
+	velocity.y = y_direction * SPEED
+	_update_animation(Vector2(x_direction,y_direction))
+	_pick_state()
 	
 
 	move_and_slide()
 	
 
-func _update_animation(x_direction, y_direction):
-	if (x_direction > 0):
-		direction = direction_enum.RIGHT
-	if (x_direction < 0):
-		direction = direction_enum.LEFT
-	if y_direction > 0:
-		direction = direction_enum.DOWN
-	if y_direction < 0:
-		direction = direction_enum.UP
-	if (x_direction == 0 && y_direction == 0):
-		$Sprite_Run.visible = false
-		$Sprite_Idle.visible = true
-		match direction:
-			direction_enum.UP:
-				_animation_player.play("idle_up")
-			direction_enum.DOWN:
-				_animation_player.play("idle_down")
-			direction_enum.RIGHT:
-				_animation_player.play("idle_right")
-			direction_enum.LEFT:
-				_animation_player.play("idle_left")
-	else:
-		$Sprite_Idle.visible = false
-		$Sprite_Run.visible = true 
-		match direction:
-			direction_enum.UP:
-				_animation_player.play("run_up")
-			direction_enum.DOWN:
-				_animation_player.play("run_down")
-			direction_enum.RIGHT:
-				_animation_player.play("run_right")
-			direction_enum.LEFT:
-				_animation_player.play("run_left")
-	_animation_player.advance(0)
+func _update_animation(direction_input : Vector2):
+	# ignore if no input
+	if(direction_input != Vector2.ZERO):
+		_animation_tree.set("parameters/Idle/blend_position", direction_input)
+		_animation_tree.set("parameters/Run/blend_position", direction_input)
 	
-
+func _pick_state():
+	if (velocity != Vector2.ZERO):
+		_animation_state_machine.travel("Run")
+	else :
+		_animation_state_machine.travel("Idle")
